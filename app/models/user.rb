e287ac3d4f36ded9ab010d6d498ca33915d0f6c8
keisukethewinter <addict20fx1@gmail.com>
2020-09-03 23:34:38 +0900

    パスワード確認のバリデーション記述漏れを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   EMAIL_REGEX = /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
   validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX}
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, presence: true, length: { minimum: 6 } , format: { with: PASSWORD_REGEX}
+  validates :password, confirmation: true, presence: true, length: { minimum: 6 } , format: { with: PASSWORD_REGEX}
+  validates :password_confirmation, presence: true
   validates :last_name, presence: true ,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
