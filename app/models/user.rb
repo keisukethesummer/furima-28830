08ea888395f167152026147c0129ca460c2f8062
keisukethewinter <addict20fx1@gmail.com>
2020-09-04 14:26:18 +0900

    ユーザー管理機能の修正・rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  EMAIL_REGEX = /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/
-  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX}
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, confirmation: true, presence: true, length: { minimum: 6 } , format: { with: PASSWORD_REGEX}
-  validates :password_confirmation, presence: true
-  validates :last_name, presence: true ,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :birthday, presence: true
+         with_options presence: true do
+            validates :nickname
+            EMAIL_REGEX = %r{\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z}.freeze
+            validates :email, uniqueness: true, format: { with: EMAIL_REGEX }
+            PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+            validates :password, confirmation: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
+            validates :password_confirmation
+            NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+            validates :last_name, format: { with: NAME_REGEX }
+            validates :first_name, format: { with: NAME_REGEX }
+            KANA_REGEX = /\A[ァ-ン]+\z/
+            validates :last_name_kana, format: { with: KANA_REGEX }
+            validates :first_name_kana, format: { with: KANA_REGEX }
+            validates :birthday
+         end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"1234abcd"}
-    password_confirmation {password}
-    first_name            {"山田"}
-    last_name             {"花子"}
-    first_name_kana       {"ヤマダ"}
-    last_name_kana        {"ハナコ"}
-    birthday              {"1995-01-01"}
+    nickname              { 'abe' }
+    email                 { 'kkk@gmail.com' }
+    password              { '1234abcd' }
+    password_confirmation { password }
+    first_name            { '山田' }
+    last_name             { '花子' }
+    first_name_kana       { 'ヤマダ' }
+    last_name_kana        { 'ハナコ' }
+    birthday              { '1995-01-01' }
   end
 end
