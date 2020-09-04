80f3629aeb9b5752b3724e9d9c46fbb4ef69495d
keisukethewinter <addict20fx1@gmail.com>
2020-09-04 15:27:39 +0900

    itemモデルの作成とitemsテーブルの各カラム作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_073355) do
+ActiveRecord::Schema.define(version: 2020_09_04_060626) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.string "image", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "category", null: false
+    t.integer "condition", null: false
+    t.integer "delivery_fee", null: false
+    t.integer "seller_prefecture", null: false
+    t.integer "shipment_day", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", limit: 191, default: "", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_09_03_073355) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
