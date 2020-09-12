f67858a588a84a16a1cc9d4d4aebc0122f8cf92d
keisukethewinter <addict20fx1@gmail.com>
2020-09-12 15:40:37 +0900

    rubocop を実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -18,12 +18,12 @@ class ItemsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
     @item = Item.find(params[:id])
     @user = User.find(@item.user_id)
   end
-  
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
