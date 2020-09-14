d5517928132043e3709afe2566dd534edc6ee068
keisukethewinter <addict20fx1@gmail.com>
2020-09-14 19:29:04 +0900

    Formオブジェクトを作成し、モデルに書いたバリデーションをこちらへ移動

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-  with_options presense: true do
-    validates :postal_number, format: {with: /\A\d{3}[-]\d{4}\z/}
-    validates :prefecture_id, numericality: { other_than: 0 }
-    validates :city
-    validates :house_number
-    validates :phone_number, length: {maximum: 11 }
-  end
-  
-  
 end
