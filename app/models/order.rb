f26e6c66cb1e3f236327b7e092e9c60c5b8bfaf9
keisukethewinter <addict20fx1@gmail.com>
2020-09-14 18:02:19 +0900

    住所テーブルを作成しアソシエーション・バリデーションを設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :seller_prefecture
   belongs_to :user
   belongs_to :item
+  has_one :address
+  
+  validates :token, presence: true
 end
