2f68566cac2b91f4904bb86c59d6fedbd8b10ba6
keisukethewinter <addict20fx1@gmail.com>
2020-09-15 16:48:45 +0900

    住所モデルとFormオブジェクトを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new
+    @order = OrderAddress.new
     @item = Item.find(params[:item_id])
   end
 
   def create
+    @order = OrderAddress.new(order_params)
+    # binding.pry
+    if @order.save
+      redirect_to root_path
+    else
+      redirect_to 'orders/index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:user_id, :item_id, :order_id, :postal_number, :prefecture_id, :city, :house_number, :build_name, :phone_number).merge(user_id: current_user.id)
   end
 end
---
+++ app/forms/order_address.rb
@@ -0,0 +1,20 @@
+class OrderAddress
+
+  include ActiveModel::Model
+  attr_accessor :order_id, :user_id, :name, :price, :item_id, :postal_number, :prefecture_id, :city, :build_name, :house_number, :phone_number
+
+  with_options presence: true do
+    validates :postal_number, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :city
+    validates :house_number
+    validates :phone_number, length: {maximum: 11 }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # binding.pry
+    address = Address.create(order_id: order.id, postal_number: postal_number, prefecture_id: prefecture_id, city: city, build_name: build_name, house_number: house_number, phone_number: phone_number)
+    # binding.pry
+  end
+end
