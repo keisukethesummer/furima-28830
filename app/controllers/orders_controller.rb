9a004b44537e2bafab6809bda2d682aa27eff788
keisukethewinter <addict20fx1@gmail.com>
2020-09-15 19:07:12 +0900

    エラーハンドリングとストロングパラメータの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,16 +7,28 @@ class OrdersController < ApplicationController
   def create
     @order = OrderAddress.new(order_params)
     # binding.pry
-    if @order.save
-      redirect_to root_path
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
     else
-      redirect_to 'orders/index'
+      @item = Item.find(params[:item_id])
+      render 'orders/index',model: @item
     end
   end
 
   private
 
   def order_params
-    params.permit(:user_id, :item_id, :order_id, :postal_number, :prefecture_id, :city, :house_number, :build_name, :phone_number).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:price, :token, :user_id, :item_id, :order_id, :postal_number, :prefecture_id, :city, :house_number, :build_name, :phone_number).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
