00c45c84578c9d79b3b42688b981ffc1152d9dba
keisukethewinter <addict20fx1@gmail.com>
2020-09-16 19:23:45 +0900

    URLに直接入力してアクセスしてもリダイレクトされる処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :signed_in, only: [:index]
+
   def index
+    @item = Item.find(params[:item_id]) 
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+    @orders = Order.all
+    @orders.each do |order|
+      if order.item_id == @item.id 
+        redirect_to root_path
+      end
+    end
     @order = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    # binding.pry
     @order = OrderAddress.new(order_params)
     if @order.valid?
       pay_item
-      # binding.pry
       @order.save
       return redirect_to root_path
     else
@@ -21,12 +30,10 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    # binding.pry
-    params.require(:order_address).permit(:token, :postal_number, :prefecture_id, :city, :house_number, :build_name, :phone_number).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_number, :prefecture_id, :city, :house_number, :build_name, :phone_number).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
 
   def pay_item
-    # binding.pry
     @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
@@ -34,5 +41,11 @@ class OrdersController < ApplicationController
       card: params[:token],
       currency: 'jpy'
     )
+  end
+
+  def signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
